d91cf7146be0ced5db02b05e531e1cb9a8d7d6d3
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2024-02-28 20:38:44 +0700

    Finished

---
+++ README.md
@@ -0,0 +1,7 @@
+# Features implemented
+### Dash mechanic
+Saya mengimplementasikan dash mechanic yang akan mengganti kecepatan pemain dengan dash speed ketika key yang sama ditekan dua kali dalam waktu yang singkat. Referensi saya dapatkan dari forum Godot
+### Double Jump
+Double Jump, saya implementasikan dengan cara menambahkan variable yang bekerja sebagai counter dan limiter player agar hanya dapat melakukan aksi melompat sebanyak dua kali saja di dalam `get_input()`. Selain itu, saya menambahkan sebuah deteksi `is_on_floor()` sebagai tempat reset counter double jump dan juga sprite.
+### Crouching
+Crouching yang saya implementasikan membuat player menunduk serta melambatkan pergerakan dari kiri atau kanan dan membuat player tidak dapat melompat.
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,6 +4,7 @@ export (int) var speed = 400
 export (int) var GRAVITY = 1200
 export (int) var jump_speed = -600
 export (int) var dash_speed = 900
+onready var sprite = $Sprite
 
 const UP = Vector2(0,-1)
 const DOUBLETAP_DELAY = .1
@@ -13,12 +14,13 @@ var double_jump = false
 
 var doubletap_time = DOUBLETAP_DELAY
 var last_keycode = 0
+var is_dashing = false
 
 func _process(delta):
 	doubletap_time -= delta
 
 func get_input():
-	
+	velocity.x = 0
 	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed
 		double_jump = true
@@ -26,23 +28,53 @@ func get_input():
 		velocity.y = jump_speed
 		double_jump = false
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += speed
+		sprite.flip_h = false
+		if is_dashing:
+			velocity.x += dash_speed
+		else:
+			velocity.x += speed
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= speed
-
+		sprite.flip_h = true
+		if is_dashing:
+			velocity.x -= dash_speed
+		else:
+			velocity.x -= speed
+	if Input.is_action_pressed("crouch"):
+		$Crouching.disabled = false
+		$Normal.disabled = true
+		
+		sprite.position.x = 0
+		sprite.position.y = 18.5
+		sprite.scale.x = 1
+		sprite.scale.y = 0.664
+		
+		speed = 200
+		dash_speed = 0
+		jump_speed = 0
+	else:
+		$Crouching.disabled = false
+		$Normal.disabled = true
+		
+		sprite.position.x = 0
+		sprite.position.y = 0
+		sprite.scale.x = 1
+		sprite.scale.y = 1
+		
+		speed = 400
+		jump_speed = -600
+		dash_speed = 900
 	
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		doubletap_time = DOUBLETAP_DELAY
 		if last_keycode == event.physical_scancode and doubletap_time >= 0: 
 			print('DoubleTap')
-			if Input.is_action_pressed('ui_right'):
-				velocity.x = dash_speed
-			if Input.is_action_pressed('ui_left'):
-				velocity.x = -dash_speed
+			is_dashing = true
 			last_keycode = 0
 		else:
+			is_dashing = false
 			last_keycode = event.physical_scancode
+
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
